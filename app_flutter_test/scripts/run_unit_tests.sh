e20bf68e760eb4a66ea380c9e1b7f25057dc28b1
RuslanTsitser <cicer_ruslan@mail.ru>
2025-12-03 21:04:22 +0300

    Enhance GitHub Actions workflow by enabling Flutter caching and improving installation script to check for existing Flutter installation before proceeding.

--- app_flutter_test/scripts/run_unit_tests.sh
+++ app_flutter_test/scripts/run_unit_tests.sh
@@ -1,10 +1,14 @@
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 
-# install flutter
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
-which flutter
+if [ ! -d "flutter" ]; then
+  echo "Flutter not found, installing..."
+  sudo apt-get update -y && sudo apt-get upgrade -y
+  sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+  git clone https://github.com/flutter/flutter.git -b stable
+  export PATH="$PATH:`pwd`/flutter/bin"
+  which flutter
+
+fi
+
 flutter doctor
 
 flutter test test/unit/custom/custom_test.dart
