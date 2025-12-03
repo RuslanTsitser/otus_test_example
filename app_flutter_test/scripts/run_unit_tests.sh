d2bac11094bdd4fb7c313fcbe689ec5e7e899745
RuslanTsitser <cicer_ruslan@mail.ru>
2025-12-03 20:58:30 +0300

    Update GitHub Actions workflow to install Flutter dependencies and run unit tests from script, enhancing setup process.

--- app_flutter_test/scripts/run_unit_tests.sh
+++ app_flutter_test/scripts/run_unit_tests.sh
@@ -1,2 +1,10 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+
+# install flutter
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+which flutter
+flutter doctor
 
 flutter test test/unit/custom/custom_test.dart
