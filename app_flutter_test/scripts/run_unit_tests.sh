c13aa39517c97fb5b7ead90d95abc0d425837812
RuslanTsitser <cicer_ruslan@mail.ru>
2025-12-03 21:11:29 +0300

    Update unit test script to ensure Flutter installation path is set correctly after cloning, improving setup reliability.

--- app_flutter_test/scripts/run_unit_tests.sh
+++ app_flutter_test/scripts/run_unit_tests.sh
@@ -1,12 +1,11 @@
-
+export PATH="$PATH:`pwd`/flutter/bin"
 if [ ! -d "flutter" ]; then
   echo "Flutter not found, installing..."
   sudo apt-get update -y && sudo apt-get upgrade -y
   sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
   git clone https://github.com/flutter/flutter.git -b stable
-  export PATH="$PATH:`pwd`/flutter/bin"
-  which flutter
 
+  export PATH="$PATH:`pwd`/flutter/bin"
 fi
 
 flutter doctor
